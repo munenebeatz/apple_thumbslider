50363d940ea6599e372cc17b61a291a68a68a441
Munenebeatz <md@cordelia.co.ke>
2016-09-07 15:15:05 +0300

    js complete

--- script.js
+++ script.js
@@ -5,5 +5,42 @@
     //page Title
     $('title').html('jQuery Slider');
 
+    //mk 1sr image active
+    $('.product').first().addClass('active');
 
+    //slider:::
+    var totalWidth = 0,
+        positions = [];
+
+    $('.slide').each(function (i) {
+        positions[i] = totalWidth;
+        totalWidth += $(this).width();
+
+        if(!($(this).width())){
+            alert('Add width to images');
+            return false;
+        }
+
+    });
+
+    $('.slides').width(totalWidth);
+
+    $('#menu ul li a').on('click', function (e, keepScroll) {
+        $('li.product').removeClass('active').addClass('inactive');
+        //Add active class to parent
+        $(this).parent().addClass('active');
+
+        var pos = $(this).parent().prevAll('.product').length;
+        console.log(pos);
+
+        $('.slides').stop().animate({
+            marginLeft : -positions[pos]+'px'
+        },450);
+
+        if(!autoScroll){
+            clearInterval(intvl);
+        }
+
+        e.preventDefault();
+    });
 })(jQuery);
